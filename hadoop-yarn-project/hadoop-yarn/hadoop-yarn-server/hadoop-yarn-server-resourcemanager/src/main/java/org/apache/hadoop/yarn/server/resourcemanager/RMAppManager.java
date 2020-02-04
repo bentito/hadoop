53627f2c44207022e3471aacd05285758b251b1f
Jonathan Hung <jhung@linkedin.com>
2020-02-04 13:52:25 -0800

    YARN-10116. Expose diagnostics in RMAppManager summary
    (cherry picked from commit 314e2f9d2e888fae1e5bf669aeeead84a928d282)
    (cherry picked from commit 147897da4b420b4749f3c7b410f4c329632c3352)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/RMAppManager.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/RMAppManager.java
@@ -218,8 +218,8 @@ public class RMAppManager implements EventHandler<RMAppManagerEvent>,
                   == null
                   ? ""
                   : app.getApplicationSubmissionContext()
-                      .getNodeLabelExpression());
-
+                      .getNodeLabelExpression())
+          .add("diagnostics", app.getDiagnostics());
       return summary;
     }
 
